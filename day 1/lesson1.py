09ea0e0ddf817ad5a8f2e111c5a6484f58206622
MateChanadiri <mate.chanadiri@gmail.com>
2024-05-19 09:58:40 +0400

    secondcommit

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -8,4 +8,12 @@ height = 151
 
 sister = "tatuli"
 
-print("cemi saxelia" + " " + name, "xolo cemi gvari ki" + " " + surname, "me var wlis-", age, "cemi simaglea",  height, "cm" " me myavs da saxelad" + " " + sister)
+mother = "gvantsa"
+
+pet_parrot = "tuta"
+
+school = "24-e sajaro skolasi"
+
+
+
+print("cemi saxelia" + " " + name, "xolo cemi gvari ki" + " " + surname, "me var wlis-", age, "cemi simaglea",  height, "cm" " me myavs da saxelad" + " " + sister, "dedacemis saxelia", mother, "me myavs tutiyushi saxelad" + " " + pet_parrot, "da me davdivar" + " " + school)
