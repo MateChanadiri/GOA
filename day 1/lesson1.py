6bd8b8a165e96358963c9323941c2f23bd793ef8
MateChanadiri <mate.chanadiri@gmail.com>
2024-05-19 09:39:49 +0400

    secondcommit

---
+++ day 1/lesson1.py
@@ -0,0 +1,11 @@
+name = "mate"
+
+surname = "chanadiri"
+
+age = 12
+
+height = 151
+
+sister = "tatuli"
+
+print("cemi saxelia" + " " + name, "xolo cemi gvari ki" + " " + surname, "me var wlis-", age, "cemi simaglea",  height, "cm" " me myavs da saxelad" + " " + sister)
