e4c5c3ff31c6efed486f3edaf41350b1ecea625a
MateChanadiri <mate.chanadiri@gmail.com>
2024-05-20 13:52:02 +0400

    secondcommit

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -14,6 +14,4 @@ pet_parrot = "tuta"
 
 school = "24-e sajaro skolasi"
 
-
-
 print("cemi saxelia" + " " + name, "xolo cemi gvari ki" + " " + surname, "me var wlis-", age, "cemi simaglea",  height, "cm" " me myavs da saxelad" + " " + sister, "dedacemis saxelia", mother, "me myavs tutiyushi saxelad" + " " + pet_parrot, "da me davdivar" + " " + school)
