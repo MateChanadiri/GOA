0f9b1cbcc5c8b9694c8b899883601712ba2e4b18
MateChanadiri <mate.chanadiri@gmail.com>
2024-06-20 15:08:42 +0400

    second commit

--- level 3/classwork/test.py
+++ level 3/classwork/test.py
@@ -2,3 +2,10 @@ name = input("whats your name? ")
 lastname = input("alright, and your lastname? ")
 fullname = name + " " + lastname
 input(f"thanks for the infortmation {fullname}")
+
+num1 = 12
+num2 = 23
+num3 = 9
+print(num1 + num2 +num3)
+print(num1 - num2 / num3)
+print(num1 + num2 - num3 + num2 / num3)
