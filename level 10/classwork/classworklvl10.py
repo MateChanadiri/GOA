28e510dcf00294a35e9e73c067d5d6100e9e5b69
MateChanadiri <mate.chanadiri@gmail.com>
2024-07-13 16:45:15 +0400

    commit

---
+++ level 10/classwork/classworklvl10.py
@@ -0,0 +1,38 @@
+age = int(input("how old are you: "))
+if age < 13:
+    print("pipsquek")
+elif age > 14:
+    print("yo we got a big fella here")
+else:
+    print("your not more than 50... right? you CANT be right?")
+
+
+house_temp = 31
+if house_temp > 30:
+    print(f"Detected temperature {house_temp}, your ac is turning on Matthew")
+
+
+
+for i in range(101):
+    print(i)
+
+for i in range(1, 11, 2): 
+    print(i)
+
+for i in range(100, 1, -1):
+    print(i)
+
+counter = 0
+while counter < 101:
+    print(counter)
+    counter = counter + 1
+
+counter = 0
+while counter < 11:
+    print(counter)
+    counter = counter + 2
+
+counter = 101
+while counter > 0:
+    print(counter)
+    counter = counter - 1
---
+++ level 10/homework/homeworklvl10.py
@@ -0,0 +1,54 @@
+#work N1
+
+for i in range(11):
+    print(i)
+    print("hello world")
+
+#work N2
+
+sum = 0
+for i in range(11):
+  sum = i + sum
+print(sum)
+
+#work N3
+ 
+for i in range(0, 1100, 100):
+    print(i)
+
+#work N4
+
+for i in range(100, 0, -1):
+    print(i)
+
+#work N5
+
+while i < 11:
+    print(i)
+    print("hello world")
+    i = i + 1
+
+#work N6
+
+sum1 = 0
+i = 1
+while i <= 11:
+  sum1 = i + sum
+  i = i + 1
+print(i)
+ 
+
+#work N7
+
+i = 0
+while i < 1100:
+    print(i)
+    i = i + 100
+
+#work N8
+
+counter = 101
+while counter > 0:
+    print(counter)
+    counter = counter - 1
+
