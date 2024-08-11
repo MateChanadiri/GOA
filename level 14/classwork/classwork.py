41e2cf17ec5360b920c0ed77c3133f53e0bb13e1
MateChanadiri <mate.chanadiri@gmail.com>
2024-08-11 21:20:14 +0400

    commit

---
+++ level 14/classwork/classwork.py
@@ -0,0 +1,61 @@
+#first work
+list1 = "Hello"
+list2 = ["My", "Name"]
+list3 = ["is", "Mate", "Chanadiri"]
+list4 = ["what", "about", "you", "friend?"]
+list5 = ["nice", "to", "finally", "meet", "you"]
+print(list1)
+print(list2[0])
+print(list2[1])
+print(list2[-2])
+print(list2[-1])
+print(list3[0])
+print(list3[1])
+print(list3[2])
+print(list3[-3])
+print(list3[-2])
+print(list3[-1])
+print(list4[0])
+print(list4[1])
+print(list4[2])
+print(list4[3])
+print(list4[-4])
+print(list4[-3])
+print(list4[-2])
+print(list4[-1])
+print(list5[0])
+print(list5[1])
+print(list5[2])
+print(list5[3])
+print(list5[4])
+print(list5[-5])
+print(list5[-4])
+print(list5[-3])
+print(list5[-2])
+print(list5[-1])
+
+#second work
+nums = [1,2,3,4,5,6,7,8,9,10]
+num1 = nums[1] + nums[9]
+num2 = nums[8] - nums[0]
+num3 = nums[5] * nums[6]
+print(nums)
+print(num1, num2, num3)
+
+#third work
+list = ["good", "is", "programming"]
+print(list[2])
+print(list[1])
+print(list[0])
+print("negative")
+print(list[-1])
+print(list[-2])
+print(list[-3])
+
+#fourth work
+numbers = [1,2,3,4]
+print(numbers)
+numbers[2] = 2
+numbers[3] = 2
+print(numbers)
+
---
+++ level 14/homework/homework.py
@@ -0,0 +1,59 @@
+       #0 #1 #2 #3 #4 #5 #6 #7 #8 #9  #10 #11 #12 #13 #14
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+#-15 #-14 #-13 #-12 #-11 #-10 #-9 #-8 #-7 #-6 #-5 #-4 #-3 #-2 #-1
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+print(list[8])
+print(list[9])
+print(list[10])
+print(list[11])
+print(list[12])
+print(list[13])
+print(list[14])
+print(list[-15])
+print(list[-14])
+print(list[-13])
+print(list[-12])
+print(list[-11])
+print(list[-10])
+print(list[-9])
+print(list[-8])
+print(list[-7])
+print(list[-6])
+print(list[-5])
+print(list[-4])
+print(list[-3])
+print(list[-2])
+print(list[-1])
+
+       #0 #1 #2 #3 #4 #5 #6 #7 #8 #9  #10 #11 #12 #13 #14
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+#-15 #-14 #-13 #-12 #-11 #-10 #-9 #-8 #-7 #-6 #-5 #-4 #-3 #-2 #-1
+print(list2[:3])
+print(list2[:6])
+print(list2[:9])
+print(list2[:12])
+print(list2[:15])
+
+list3 = [1,2,3,4,5]
+print(list3)
+list3[0] = 2
+list3[1] = 3
+list3[2] = 3
+list3[3] = 4
+list3[4] = 5
+print(list3)
+
+list4 = [1, 2, 3, 4, 5]
+print(list4)
+list4[1:3] = [10, 20]
+print(list4)
+
+
+
